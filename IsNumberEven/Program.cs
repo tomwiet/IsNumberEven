e3eb0c3e07df8c248a3d6fc94a3b10666aa4d981
tomwiet <tthanquol@gmail.com>
2023-10-31 16:22:09 +0100

    Dodaj pliki projektów.

---
+++ IsNumberEven/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsNumberEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
